b1958ec4b0b4f85479a0c58c1a4eaa2e168ad303
quietcry <quietcry@gmx.de>
2026-01-05 14:49:13 +0100

    Version 2026.01-0007: Editor unabhängig von Saver-Config, Entity-Prüfung nur bei text_entity Modus

--- src/card-config.js
+++ src/card-config.js
@@ -1,4 +1,4 @@
-const Version = '2026.01-0005';
+const Version = '2026.01-0007';
 const CardRegname = 'tgshiftschedule-card';
 const CardName = 'TG Schichtplan Card';
 const CardDescription = 'Eine Schichtplan-Karte für Arbeitszeiten';
